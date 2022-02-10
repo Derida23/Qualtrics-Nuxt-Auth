8344db27394590897f8acdc8747c82a8469b8c36
Arian Derida <arianderida@gmail.com>
2022-02-10 11:58:53 +0700

    feat(homepage): slicing html css for homepage

---
+++ components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home d-flex justify-center align-center">
+    <div>
+      <QuitLogo />
+      <v-row align="center" justify="center" class="mt-10">
+        <v-btn elevation="2" class="ma-2 white--text" color="#1890FF" large
+          >Register <v-icon>mdi-account</v-icon>
+        </v-btn>
+
+        <v-btn elevation="2" class="ma-2 white--text" color="#1890FF" large
+          >Login <v-icon>mdi-login-variant</v-icon>
+        </v-btn>
+      </v-row>
+    </div>
+  </div>
+</template>
+
+<script>
+import QuitLogo from '@/components/QuitLogo'
+
+export default {
+  name: 'HomePage',
+  components: {
+    QuitLogo,
+  },
+}
+</script>
+
+<style lang="scss">
+.home {
+  min-height: 80vh;
+}
+</style>
---
+++ components/QuitLogo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="quit-logo d-flex align-center justify-center">
+    <img src="/assets/qualtrics-logo.png" alt="logo" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuitLogo',
+  // props: {
+  //   value: { type: String, default: '' },
+  //   user: { type: Function, default: () => {}, require: true },
+  // },
+  // data() {
+  //   return {
+  //     count: 0,
+  //   }
+  // },
+  // methods: {
+  //   addCount() {
+  //     this.count++
+  //     this.$emit('counter', { value: this.count })
+  //   },
+  // },
+}
+</script>
+
+<style lang="scss">
+.quit-logo {
+  img {
+    height: 140px;
+  }
+}
+</style>
